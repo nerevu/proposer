6f5f7e95481d5e4bfbadefd59385ce1e5f089185
Reuben Cummings <reubano@gmail.com>
2014-07-12 13:22:06 +0300

    Modularize template sections

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ _user = os.environ.get('USER', os.environ.get('USERNAME'))
 # configurable vars
 __APP_NAME__ = 'Proposer'
 __YOUR_NAME__ = 'Reuben Cummings'
+__YOUR_COMPANY__ = 'Freelancer'
+__YOUR_POSITION__ = 'Developer'
 __YOUR_EMAIL__ = '%s@gmail.com' % _user
 __YOUR_PHONE__ = ['0789-477-319', '0756-477-319']
 __YOUR_WEBSITE__ = 'http://%s.github.io' % _user
@@ -17,9 +19,13 @@ __YOUR_WEBSITE__ = 'http://%s.github.io' % _user
 app = __APP_NAME__.lower()
 year = d.today().strftime("%Y")
 date = d.today().strftime("%B %d, %Y")
-site_keys = ('author', 'author_email', 'author_phone', 'author_url', 'year', 'date')
+site_keys = (
+	'author', 'author_company', 'author_position', 'author_email',
+	'author_phone', 'author_url', 'year', 'date')
+
 site_values = (
-	__YOUR_NAME__, __YOUR_EMAIL__, __YOUR_PHONE__, __YOUR_WEBSITE__, year, date)
+	__YOUR_NAME__, __YOUR_COMPANY__, __YOUR_POSITION__, __YOUR_EMAIL__,
+	__YOUR_PHONE__, __YOUR_WEBSITE__, year, date)
 
 # configuration
 class Config(object):
