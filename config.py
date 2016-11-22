5823220a396dbf7ad1c41bc99ab03061a3df237a
Reuben Cummings <reubano@gmail.com>
2016-11-22 09:48:41 +0300

    Update app and templates

--- config.py
+++ config.py
@@ -9,11 +9,12 @@ _user = os.environ.get('USER', os.environ.get('USERNAME'))
 # configurable vars
 __APP_NAME__ = 'Proposer'
 __YOUR_NAME__ = 'Reuben Cummings'
-__YOUR_COMPANY__ = 'Freelancer'
-__YOUR_POSITION__ = 'Developer'
-__YOUR_EMAIL__ = '%s@gmail.com' % _user
-__YOUR_PHONE__ = ['0789-477-319', '0756-477-319']
-__YOUR_WEBSITE__ = 'http://%s.github.io' % _user
+__YOUR_COMPANY__ = 'Nerevu Development'
+__YOUR_POSITION__ = 'Managing Director'
+__YOUR_EMAIL__ = 'rcummings@nerevu.com'
+__YOUR_SKYPE__ = _user
+__YOUR_PHONE__ = ['+255 789 477 319', '+254 703 576 035']
+__YOUR_WEBSITE__ = 'http://nerevu.com'
 
 # calculated vars
 app = __APP_NAME__.lower()
@@ -21,11 +22,11 @@ year = d.today().strftime("%Y")
 date = d.today().strftime("%B %d, %Y")
 site_keys = (
 	'author', 'author_company', 'author_position', 'author_email',
-	'author_phone', 'author_url', 'year', 'date')
+	'author_skype', 'author_phone', 'author_url', 'year', 'date')
 
 site_values = (
 	__YOUR_NAME__, __YOUR_COMPANY__, __YOUR_POSITION__, __YOUR_EMAIL__,
-	__YOUR_PHONE__, __YOUR_WEBSITE__, year, date)
+	__YOUR_SKYPE__, __YOUR_PHONE__, __YOUR_WEBSITE__, year, date)
 
 # configuration
 class Config(object):
